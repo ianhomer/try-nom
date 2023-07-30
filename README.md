3cdfc50e91148dfb0be0f27ef3d86dce76c85b5c
Ian Homer <code@ianhomer.com>
2023-07-30 21:31:54 +0100

    no op nom

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# try-nom
-Try nom
+ # Try nom
+
+Try [nom](https://tfpk.github.io/nominomicon/chapter_1.html)
+
+    cargo build
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
 use std::env;
 use std::fs;
 
+use nom::IResult;
+
+pub fn parse(file_path: &str) -> IResult<&str, &str> {
+    let contents = fs::read(file_path).expect("Should have been able to read the file");
+    let len = contents.len();
+
+    println!("Read file with length {len}");
+    Ok(("xx", ""))
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let file_path = &args[1];
 
     println!("In file {}", file_path);
-
-    fs::read(file_path)
-        .expect("Should have been able to read the file");
-
-    println!("Read file");
-    //println!("With text:\n{contents}");
+    let result = parse(file_path);
+    println!("Result {result:?}")
 }
